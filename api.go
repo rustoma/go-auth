d98a614dd852b839f2a944b86b202666ad2018dc
rustoma <rutoma00@gmail.com>
2023-06-30 20:36:45 +0200

    feat: add routes to api.go

--- api.go
+++ api.go
@@ -3,13 +3,61 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"io"
+	"log"
 	"net/http"
 )
+
+type APIServer struct {
+	listenAddr string
+	store      Storage
+}
+
+type CreateUserRequest struct {
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
+}
 
 type LoginRequest struct {
 	UserName string `json:"user_name"`
 	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	AccessToken string `json:"access_token"`
+}
+
+func NewApiServer(listenAddr string, storage Storage) *APIServer {
+	return &APIServer{
+		listenAddr,
+		storage,
+	}
+}
+
+func (s *APIServer) Run() {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	//mux.Use(app.enableCORS)
+
+	mux.Get("/api/v1/refresh", makeHTTPHandler(s.HandleRefreshToken))
+	mux.Get("/api/v1/logout", makeHTTPHandler(s.HandleLogout))
+
+	mux.Post("/api/v1", s.requireAuth(2, 3)(makeHTTPHandler(s.HandleHome)))
+	mux.Post("/api/v1/login", makeHTTPHandler(s.HandleLogin))
+
+	mux.Post("/api/v1/users", makeHTTPHandler(s.HandleCreateUser))
+
+	log.Println("JSON API server running on port: ", s.listenAddr)
+
+	//err := http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), mux)
+	err := http.ListenAndServe(s.listenAddr, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 func makeHTTPHandler(f apiFunc) http.HandlerFunc {
