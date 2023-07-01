e244c3e678d8237e9651b7413e191a83cd1f9b8e
rustoma <rutoma00@gmail.com>
2023-07-01 07:02:53 +0200

    feat: add storage with functions related to auth

--- storage.go
+++ storage.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
@@ -12,6 +13,9 @@ import (
 
 type Storage interface {
 	InsertUser(user User) (int, error)
+	SelectUserByUserName(userName string) (*User, error)
+	SelectUserByRefreshToken(refreshToken string) (*User, error)
+	UpdateUserRefreshToken(userId int, refreshToken string) (int, error)
 }
 
 type PostgresStorage struct {
@@ -48,6 +52,7 @@ func (s *PostgresStorage) Init() error {
 							id SERIAL PRIMARY KEY,
 							user_name VARCHAR(255),
 							password VARCHAR(255),
+							refresh_token VARCHAR(255),
 							created_at TIMESTAMPTZ,
 							updated_at TIMESTAMPTZ
 							)`
@@ -79,4 +84,111 @@ func (s *PostgresStorage) InsertUser(user User) (int, error) {
 
 	return userID, err
 
+}
+
+func (s *PostgresStorage) SelectUserByUserName(userName string) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), s.timeout)
+	defer cancel()
+
+	selectUserStmt := `select id, user_name, password, COALESCE(refresh_token, '') AS refresh_token, created_at, updated_at
+			from 
+			    public.user
+			where 	
+				user_name = $1
+			`
+
+	rows, err := s.db.Query(ctx, selectUserStmt, userName)
+	defer rows.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var user *User
+
+	for rows.Next() {
+		userFromScan, err := scanToUser(rows)
+
+		if err != nil {
+			return nil, err
+		}
+		user = userFromScan
+	}
+
+	if user == nil {
+		return nil, errors.New("no user found")
+	}
+
+	return user, err
+}
+
+func (s *PostgresStorage) SelectUserByRefreshToken(refreshToken string) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), s.timeout)
+	defer cancel()
+
+	selectUserStmt := `select id, user_name, password, COALESCE(refresh_token, '') AS refresh_token, created_at, updated_at
+			from 
+			    public.user
+			where 	
+				refresh_token = $1
+			`
+
+	rows, err := s.db.Query(ctx, selectUserStmt, refreshToken)
+	defer rows.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var user *User
+
+	for rows.Next() {
+		userFromScan, err := scanToUser(rows)
+
+		if err != nil {
+			return nil, err
+		}
+		user = userFromScan
+	}
+
+	if user == nil {
+		return nil, errors.New("no user found")
+	}
+
+	return user, err
+}
+
+func (s *PostgresStorage) UpdateUserRefreshToken(userId int, refreshToken string) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), s.timeout)
+	defer cancel()
+
+	updateRefreshTokenStmt := `
+		update 
+		    public.user
+		set 
+		    refresh_token = $2
+		where 
+		    id = $1
+		returning 
+		    id
+	`
+	var updatedUserId int
+
+	err := s.db.QueryRow(ctx, updateRefreshTokenStmt, userId, refreshToken).Scan(&updatedUserId)
+
+	return updatedUserId, err
+}
+
+func scanToUser(rows pgx.Rows) (*User, error) {
+	var user User
+	err := rows.Scan(
+		&user.ID,
+		&user.UserName,
+		&user.Password,
+		&user.RefreshToken,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	return &user, err
 }
